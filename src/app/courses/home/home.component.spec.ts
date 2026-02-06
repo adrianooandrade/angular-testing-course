bf8873b848f6cb6ce4f2adb355d0a22ef17031c5
Adriano Andrade <allonan.619@gmail.com>
2026-02-06 16:33:39 +0000

    Enhanced HomeComponent tests: added complete initialization and tab display logic with mocked CoursesService and async handling

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -19,50 +19,106 @@ import { HttpClient } from "@angular/common/http";
 import { COURSES } from "../../../../server/db-data";
 import { setupCourses } from "../common/setup-test-data";
 import { By } from "@angular/platform-browser";
-import { of } from "rxjs";
+import { async, of } from "rxjs";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { click } from "../common/test-utils";
+import { Course } from "../model/course";
 
 describe("HomeComponent", () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component: HomeComponent;
   let el: DebugElement;
+  let coursesService: jasmine.SpyObj<CoursesService>;
 
-  beforeEach(waitForAsync(() => {
-    const coursesService = jasmine.createSpyObj("CoursesService", [
-      "findAllCourses",
-    ]);
+  const beginnerCourses: Course[] = setupCourses().filter(
+    (course) => course.category === "BEGINNER",
+  );
 
-    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
-    TestBed.configureTestingModule({
+  const advancedCourses: Course[] = setupCourses().filter(
+    (course) => course.category === "ADVANCED",
+  );
+
+  beforeEach(async () => {
+    const coursesServiceSpy = jasmine.createSpyObj<CoursesService>(
+      "CoursesService",
+      ["findAllCourses"],
+    );
+
+    await TestBed.configureTestingModule({
       imports: [CoursesModule, NoopAnimationsModule],
-      providers: [{ provide: CoursesService, useValue: coursesService }],
-    })
-      .compileComponents()
-      .then(() => {
-        fixture = TestBed.createComponent(HomeComponent);
-        component = fixture.componentInstance;
-        el = fixture.debugElement;
-      });
-  }));
+      providers: [{ provide: CoursesService, useValue: coursesServiceSpy }],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    el = fixture.debugElement;
+    coursesService = TestBed.inject(
+      CoursesService,
+    ) as jasmine.SpyObj<CoursesService>;
+  });
 
   it("should create the component", () => {
     expect(component).toBeTruthy();
   });
 
   it("should display only beginner courses", () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-mdc-tab"));
+
+    expect(tabs.length).toBe(1, "There should be only one tab");
   });
 
   it("should display only advanced courses", () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(advancedCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-mdc-tab"));
+
+    expect(tabs.length).toBe(1, "There should be only one tab");
   });
 
   it("should display both tabs", () => {
-    pending();
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css(".mat-mdc-tab"));
+    expect(tabs.length).toBe(2, "There should be two tabs");
   });
 
-  it("should display advanced courses when tab clicked", () => {
-    pending();
+  it("should display advanced courses when tab clicked", (done: DoneFn) => {
+    coursesService.findAllCourses.and.returnValue(of(setupCourses()));
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css(".mat-mdc-tab"));
+
+    expect(tabs.length).toBe(2, "There should be two tabs");
+
+    tabs[1].nativeElement.click();
+    fixture.detectChanges();
+
+    setTimeout(() => {
+      const cardTitles = el.queryAll(By.css(".mat-mdc-card-title"));
+
+      console.log(cardTitles);
+
+      typeof expect(cardTitles.length).toBe(
+        3,
+        "There should be more than one card",
+      );
+
+      const selected = el.nativeElement.querySelector(
+        "[role='tab'][aria-selected='true']",
+      );
+
+      expect(selected.textContent).toContain(
+        "Advanced",
+        "Advanced tab should be selected",
+      );
+
+      done();
+    }, 500);
   });
 });
